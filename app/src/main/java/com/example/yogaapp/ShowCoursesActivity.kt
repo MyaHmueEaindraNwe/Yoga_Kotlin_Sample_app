537b46dece9882d2ca165c8db594d353382d9696
Mya Hmue <myahmue302@gmail.com>
2025-09-04 23:16:38 +0630

    update button

--- app/src/main/java/com/example/yogaapp/ShowCoursesActivity.kt
+++ app/src/main/java/com/example/yogaapp/ShowCoursesActivity.kt
@@ -3,12 +3,12 @@ package com.example.yogaapp
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.yogaapp.databinding.ActivityShowCoursesBinding
 
@@ -36,7 +36,47 @@ class ShowCoursesActivity : AppCompatActivity() {
     }
 
     private fun editAction(yogaCourse: YogaCourse){
+        val alertDialog = AlertDialog.Builder(this)
 
+        val dialogLayout = LayoutInflater.from(this).inflate(R.layout.edit_course_layout,null)
+        alertDialog.setView(dialogLayout)
+
+        val days = dialogLayout.findViewById<EditText>(R.id.editTextUpdateDow)
+        val capacity = dialogLayout.findViewById<EditText>(R.id.editTextCapacity)
+        val duration = dialogLayout.findViewById<EditText>(R.id.editTextDuration)
+        val price = dialogLayout.findViewById<EditText>(R.id.editTextPrice)
+        val btnUpdate = dialogLayout.findViewById<Button>(R.id.btnUpdateCourseDialog)
+
+        days.setText(yogaCourse.dayOfWeek)
+        capacity.setText(yogaCourse.capacity.toString())
+        price.setText(yogaCourse.price.toString())
+        duration.setText(yogaCourse.duration.toString())
+
+        btnUpdate.setOnClickListener{
+            val newYogaCourse = YogaCourse(
+                yogaCourse.id,
+                days.text.toString(),
+                yogaCourse.timeOfCourse,
+                capacity.text.toString().toInt(),
+                duration.text.toString().toInt(),
+                price.text.toString().toInt(),
+                yogaCourse.typeOfClass,
+                yogaCourse.description)
+
+            val result = dbHelper.editCourse(newYogaCourse)
+            if(result == 0) {
+                Toast.makeText(this,"Edited Error!", Toast.LENGTH_LONG).show()
+            }
+            else {
+                Toast.makeText(this,"Edited Course", Toast.LENGTH_LONG).show()
+            }
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val alert = alertDialog.create()
+        alert.show()
     }
 
     private fun deleteAction(courseID: Int){
